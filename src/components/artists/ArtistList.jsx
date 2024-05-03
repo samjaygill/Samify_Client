063459125c2a6626404e724841e6c1eab91db2ee
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-05-03 19:15:56 +0100

    sorted artists in alphabetical order

--- src/components/artists/ArtistList.jsx
+++ src/components/artists/ArtistList.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import Artists from './Artists';
 
 function ArtistList({ artists }) {
+  const sortedArtists = artists.slice().sort((a, b) => {
+    const nameA = a.stageName.toUpperCase(); 
+    const nameB = b.stageName.toUpperCase(); 
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0; 
+  });
   return (
     <div>
-      {artists.map((artist, index) => (
+      {sortedArtists.map((artist, index) => (
         <Artists key={artist.id} artist={artist} index={index} />
       ))}
     </div>
